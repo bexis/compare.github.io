e2b16b4c43bc113b54c004ffd75ab3f480df46ee
Franzi <franzi82@gmx.de>
2023-04-16 00:39:08 +0200

    Set navigation based on URL #20

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import { afterNavigate } from '$app/navigation';
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 	import Header from '$lib/Header.svelte';
 	import Navigation from '$lib/Navigation.svelte';
+	import { storeCurrentUrl } from '../store/store';
+
+	// Scroll heading into view
+	function scrollHeadingIntoView(): void {
+		if (!window.location.hash) return;
+		const elemTarget: HTMLElement | null = document.querySelector(window.location.hash);
+		if (elemTarget) elemTarget.scrollIntoView({ behavior: 'smooth' });
+	}
+
+
+	afterNavigate((params: any) => {
+		// Store current page route URL
+		storeCurrentUrl.set($page.url.pathname);
+		// Scroll to top
+		const isNewPage: boolean = params.from && params.to && params.from.route.id !== params.to.route.id;
+		const elemPage = document.querySelector('#page');
+		if (isNewPage && elemPage !== null) {
+			elemPage.scrollTop = 0;
+		}
+		// Scroll heading into view
+		scrollHeadingIntoView();
+	});
+
+
+
 </script>
 
 <AppShell
